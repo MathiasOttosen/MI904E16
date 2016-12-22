3f97f7283ac22b40e237ccbcdf0e92365a216797
Andrea <andrea.gradecak@fer.hr>
2016-12-22 02:59:00 +0100

    final cnn

--- cnn/basic_cnn.py
+++ cnn/basic_cnn.py
@@ -2,15 +2,20 @@ import cv2
 import tensorflow as tf
 import csv
 import numpy as np
+import tensorflow.contrib.slim as slim
+import sklearn.metrics
 
 DATASET_PATH = '/media/andrea/New Volume/train/train'
-TRAIN_PATH = '/home/andrea/Documents/project/MI904E16/cnn/dataset_files/600_train_subset.csv'
-VALID_PATH = '/home/andrea/Documents/project/MI904E16/cnn/dataset_files/600_valid_subset.csv'
-TEST_PATH = '/home/andrea/Documents/project/MI904E16/cnn/dataset_files/600_test_subset.csv'
+TRAIN_PATH = '/home/andrea/Documents/project/MI904E16/cnn/dataset_files/final_train.csv'
+TEST_PATH = '/home/andrea/Documents/project/MI904E16/cnn/dataset_files/final_test.csv'
 
-BATCH_SIZE = 50
-NUM_OF_CLASSES = 5
-CROP = 64
+BATCH_SIZE = 40
+NUM_OF_CLASSES = 25
+CROP = 256
+
+LOAD_VARS = False
+TRAIN = True
+TEST = True
 
 def decode_csv(csv_path):
     f = open(csv_path, 'rt')
@@ -95,8 +100,6 @@ class DataSet(object):
             perm = np.arange(self._num_examples, dtype=np.int32)
             np.random.shuffle(perm)
             self._images_paths = self._images_paths[perm]
-            #print(self._labels.get_shape())
-            #print(type(perm))
             self._labels = self._labels[perm]
             # Start next epoch
             start = 0
@@ -116,7 +119,7 @@ class DataSet(object):
         #return tf.pack(one_hot_labels)
         return np.array(one_hot_labels, dtype=np.int32)
 
-    def center_crop(self, image, desired_height=256, desired_width=256):
+    def resize(self, image, desired_height=256, desired_width=256):
 
         image = cv2.resize(image, (desired_height, desired_width))
         image = np.reshape(image, [np.shape(image)[0] * np.shape(image)[1]])
@@ -129,7 +132,7 @@ class DataSet(object):
         for p in img_paths:
             image = cv2.imread(p,0)
             if self._preprocess:
-                image = self.center_crop(image=image, desired_width=CROP, desired_height=CROP)
+                image = self.resize(image=image, desired_width=CROP, desired_height=CROP)
             images.append(image)
         return np.array(images)
 
@@ -142,85 +145,131 @@ sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None, CROP*CROP])
 y_ = tf.placeholder(tf.float32, shape=[None, NUM_OF_CLASSES])
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-
-def convolutional_layer(input, stride, channels, output_depth):
-    W_conv = weight_variable([stride, stride, channels, output_depth])
-    b_conv = bias_variable([output_depth])
-
-    h_conv = tf.nn.relu(conv2d(input, W_conv) + b_conv)
-
-    return h_conv
-
-def max_pool_layer(input):
-    return max_pool_2x2(input)
-
 # because input in conv2d must have shape [batch, width, height, channels]
 # btw this doesn't make sense if the reshape is False, then he reshapes it again, silly
 x_image = tf.reshape(x, [-1,CROP,CROP,1])
 
-conv1 = convolutional_layer(x_image, 5, 1, 64)
-maxpool1 = max_pool_layer(conv1)
+FILTER = 10
+STRIDE = 5
+NUM_OF_MAX_POOL = 2
+CHANNELS = 1
+#OUTPUT_SIZE = CROP/2**NUM_OF_MAX_POOL
+OUTPUT_SIZE = 3
 
-conv2 = convolutional_layer(maxpool1, 5, 64, 128)
-maxpool2 = max_pool_layer(conv2)
+def network(net):
+    with tf.variable_scope('Network'), slim.arg_scope([slim.conv2d, slim.fully_connected],
+                      activation_fn=tf.nn.relu,
+                      weights_initializer=tf.truncated_normal_initializer(0.0, 0.01)):
+        net = slim.repeat(net, 2, slim.conv2d, 8, [3, 3], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.repeat(net, 2, slim.conv2d, 16, [3, 3], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.repeat(net, 2, slim.conv2d, 32, [3, 3], scope='conv3')
+        net = slim.max_pool2d(net, [2, 2], scope='pool3')
+        net = slim.flatten(net)
+        net = slim.fully_connected(net, 1024, scope='fc6')
+        net = slim.fully_connected(net, NUM_OF_CLASSES, activation_fn=None, scope='fc8')
 
-# conv3 = convolutional_layer(maxpool2, 5, 16, 32)
-# maxpool3 = max_pool_layer(conv3)
-#
-# conv4 = convolutional_layer(maxpool3, 5, 32, 64)
-# maxpool4 = max_pool_layer(conv4)
-#
-# conv5 = convolutional_layer(maxpool4, 5, 64, 128)
-# maxpool5 = max_pool_layer(conv5)
+        return net
 
-W_fc1 = weight_variable([16 * 16 * 128, 1024])
-b_fc1 = bias_variable([1024])
+def network1(net):
+    with tf.variable_scope('Network'), slim.arg_scope([slim.conv2d],activation_fn=tf.nn.relu,
+                      weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
+                                                      weights_regularizer=slim.l2_regularizer(0.0005)):
+        dims = 8  # bilo 48
+        net = slim.conv2d(net, dims / 2, 7, 2)
+        net = slim.max_pool2d(net,[2,2])
+        net = slim.conv2d(net, dims, 5)
+        net = slim.max_pool2d(net, [2,2])
 
-h_pool2_flat = tf.reshape(maxpool2, [-1, 16*16*128])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        net = slim.conv2d(net, dims, 3)
+        net = slim.conv2d(net, dims, 3)
+        net = slim.conv2d(net, dims, 3)
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+        net = slim.flatten(net)
 
-W_fc2 = weight_variable([1024, NUM_OF_CLASSES])
-b_fc2 = bias_variable([NUM_OF_CLASSES])
+        net = slim.fully_connected(net, 25)
+        net = slim.fully_connected(net, NUM_OF_CLASSES, activation_fn=None)
 
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        return net
+
+y_conv = network1(x_image)
+
+saver = tf.train.Saver()
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
-for i in range(600):
-  imgs, lbls = train_data.next_batch(BATCH_SIZE)
-  #if i%100 == 0:
-  if True:
-    train_accuracy = accuracy.eval(feed_dict={
-        x:imgs, y_: lbls, keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-  train_step.run(feed_dict={x: imgs, y_: lbls, keep_prob: 0.5})
+
+epochs = 0
+processed_examples = 0
+
+LOAD_MODEL_PATH = ""
+
+if LOAD_VARS:
+    saver.restore(sess, LOAD_MODEL_PATH)
+else:
+    sess.run(tf.initialize_all_variables())
+
+SAVE_MODEL_PATH = ""
+if TRAIN:
+    accs = []
+    losses = []
+    #14570 iters = 10 epohs
+    for i in range(14570):
+      imgs, lbls = train_data.next_batch(BATCH_SIZE)
+      processed_examples += BATCH_SIZE
+      if processed_examples >= train_data.num_examples:
+          processed_examples -= train_data.num_examples
+          epochs += 1
+          print("EPOCH" + str(epochs) + " REACHED")
+          save_path = saver.save(sess, SAVE_MODEL_PATH)
+          print("Model saved in file: %s" % save_path)
+      if i%1000 == 0:
+          save_path = saver.save(sess, SAVE_MODEL_PATH)
+          print("Model saved in file: %s" % save_path)
+      if True:
+          train_accuracy, loss = sess.run([accuracy, cross_entropy], feed_dict={x:imgs, y_: lbls})
+          accs.append(train_accuracy)
+          losses.append(loss)
+          print("step %d, training accuracy %g"%(i, train_accuracy))
+          print("step %d, loss %g"%(i, loss))
+      train_step.run(feed_dict={x: imgs, y_: lbls})
+
+    with open('accuracies2.txt', 'a') as f:
+        for a in accs:
+            f.write("%s\n" % a)
+
+    with open('losses2.txt', 'a') as f:
+        for l in losses:
+            f.write("%s\n" % l)
 
 
-img_paths_test, img_labels_test = decode_csv(TEST_PATH)
-test_data = DataSet(img_paths_test, img_labels_test, one_hot=True, reshape=True)
-test_batch = test_data.next_batch(test_data.num_examples)
+TEST_DIVISION = 463
+if TEST:
+    img_paths_test, img_labels_test = decode_csv(TEST_PATH)
+    test_data = DataSet(img_paths_test, img_labels_test, one_hot=True, reshape=True)
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: test_batch[0], y_: test_batch[1], keep_prob: 1.0}))
+    TEST_BATCH_SIZE = test_data.num_examples / TEST_DIVISION
 
+    y_p = tf.argmax(y_conv, 1)
+    y_true = img_labels_test
+    test_accs = []
+    y_predicted = []
+    for i in range(TEST_DIVISION):
+        test_batch = test_data.next_batch(TEST_BATCH_SIZE)
+        test_acc, y_pred, y_bla = sess.run([accuracy, y_p, tf.nn.softmax(y_conv)],feed_dict={x: test_batch[0], y_: test_batch[1]})
+        y_predicted.extend(y_pred)
+        print("%i Test accuracy %g"% (i,test_acc))
+
+        test_accs.append(test_acc)
+
+    precision = sklearn.metrics.precision_score(y_true, y_predicted, average='weighted')
+    recall = sklearn.metrics.recall_score(y_true, y_predicted, average='weighted')
+    f1_score = sklearn.metrics.f1_score(y_true, y_predicted, average='weighted')
+    print("ACCURACY " + str(sum(test_accs)/TEST_DIVISION))
+    print("PRECISION " + str(precision))
+    print("RECALL " + str(recall))
+    print("F1 " + str(f1_score))
+    print sklearn.metrics.confusion_matrix(y_true, y_predicted)
